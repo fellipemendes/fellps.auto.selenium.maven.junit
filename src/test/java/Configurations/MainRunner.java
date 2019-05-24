68353b1c32fdb5bd173a4f78eb2f44d40d541c79
Fellipe Mendes <fellipe.mendes90@hotmail.com>
2019-05-24 16:21:52 -0300

    MainRunner JenkinsFile

--- src/test/java/Configurations/MainRunner.java
+++ src/test/java/Configurations/MainRunner.java
@@ -14,6 +14,7 @@ import cucumber.api.junit.Cucumber;
         plugin = {
                 "pretty", "junit:target/rel.xml" ,
                 "json:target/cucumber.json",
+                "json:target/allure-results/AllureTestReport.json"
         }
 )
 
