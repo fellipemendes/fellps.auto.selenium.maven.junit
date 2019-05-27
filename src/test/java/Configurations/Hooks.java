e8461d0bbbef097c399801a9923d29976898667a
Fellipe Mendes <fellipe.mendes90@hotmail.com>
2019-05-27 10:51:59 -0300

    debug

--- src/test/java/Configurations/Hooks.java
+++ src/test/java/Configurations/Hooks.java
@@ -60,12 +60,15 @@ public class Hooks{
             //options.addArguments("--headless", "window-size=1366,768", "--no-sandbox");
             options.addArguments("window-size=1366,768");
             capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+            System.out.println("override 2");
             try {
                 driver.set(new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), capabilities));
+                System.out.println("override 3");
             } catch (Exception e) {
                 System.out.println("erro driver set: " + e.getMessage());
                 e.printStackTrace();
             }
+            System.out.println("override 4");
             return new ChromeDriver(options); // can be replaced with other browser drivers
             //return new InternetExplorerDriver();
             //return new ChromeDriver(); // can be replaced with other browser drivers
@@ -91,59 +94,57 @@ public class Hooks{
         System.out.println("passou navegador 2");
         //driver = new ChromeDriver();
         //ObjFact.start();
-        /*
-        try {
-            if (Navegador == "C" && Headless == true) {
-                if (Headless) {
-                    ChromeOptions option = new ChromeOptions();
-                    option.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
-                    option.addArguments("disable-popup-blocking");
-                    //driver = new ChromeDriver(option);
-                }
-            }else if (Navegador == "C" && Headless == false) {
-                System.out.println("passou navegador");
-                Hooks.getInstance().getDriver();
+//        try {
+//            if (Navegador == "C" && Headless == true) {
+//                if (Headless) {
+//                    ChromeOptions option = new ChromeOptions();
+//                    option.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
+//                    option.addArguments("disable-popup-blocking");
+//                    //driver = new ChromeDriver(option);
+//                }
+//            }else if (Navegador == "C" && Headless == false) {
+//                System.out.println("passou navegador");
+//                Hooks.getInstance().getDriver();
+//
+//
+//
+//                //driver = new ChromeDriver();
+//
+//				/*
+//				ChromeDriverService cdservice = new ChromeDriverService.Builder().build();
+//				cdservice.start();
+//				ChromeOptions options = new ChromeOptions();
+//				options.setExperimentalOption("detach", true);
+//				driver = new ChromeDriver(cdservice,options);
+//				*/
+//
+//            }else if (Navegador == "F" && Headless == false) {
+//                if (CI) {
+//                    DesiredCapabilities dc = DesiredCapabilities.firefox();
+//                    dc.setCapability("screenResolution", "1366x768");
+//                    //driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dc);
+//                }else {
+//                    System.setProperty("webdriver.gecko.driver", "C:\\uteis\\ToolsQA\\WebDrivers\\geckodriver.exe");
+//                    FirefoxOptions options = new FirefoxOptions();
+//                    options.setBinary("C:\\Program Files\\Firefox Developer Edition\\firefox.exe");
+//                    //driver = new FirefoxDriver(options);
+//                }
+//            }else if (Navegador == "F" && Headless == true) {
+//                FirefoxBinary firefoxBinary = new FirefoxBinary();
+//                firefoxBinary.addCommandLineOptions("--headless");
+//                System.setProperty("webdriver.gecko.driver", "C:\\uteis\\ToolsQA\\WebDrivers\\geckodriver.exe");
+//                FirefoxOptions firefoxOptions = new FirefoxOptions();
+//                firefoxOptions.setBinary(firefoxBinary);
+//                //driver = new FirefoxDriver(firefoxOptions);
+//            }else if (Navegador == "IE") {
+//                Hooks.getInstance().getDriver();
+//                //InternetExplorerDriver driver = new InternetExplorerDriver();
+//            }
+//            new Dimension(1366, 768);
+//        } catch (Exception e) {
+//            System.out.println("--------ERRO Navegador-----------" + e.getMessage());
+//        }
 
-
-
-                //driver = new ChromeDriver();
-
-				/*
-				ChromeDriverService cdservice = new ChromeDriverService.Builder().build();
-				cdservice.start();
-				ChromeOptions options = new ChromeOptions();
-				options.setExperimentalOption("detach", true);
-				driver = new ChromeDriver(cdservice,options);
-				*/
-        /*
-            }else if (Navegador == "F" && Headless == false) {
-                if (CI) {
-                    DesiredCapabilities dc = DesiredCapabilities.firefox();
-                    dc.setCapability("screenResolution", "1366x768");
-                    //driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dc);
-                }else {
-                    System.setProperty("webdriver.gecko.driver", "C:\\uteis\\ToolsQA\\WebDrivers\\geckodriver.exe");
-                    FirefoxOptions options = new FirefoxOptions();
-                    options.setBinary("C:\\Program Files\\Firefox Developer Edition\\firefox.exe");
-                    //driver = new FirefoxDriver(options);
-                }
-            }else if (Navegador == "F" && Headless == true) {
-                FirefoxBinary firefoxBinary = new FirefoxBinary();
-                firefoxBinary.addCommandLineOptions("--headless");
-                System.setProperty("webdriver.gecko.driver", "C:\\uteis\\ToolsQA\\WebDrivers\\geckodriver.exe");
-                FirefoxOptions firefoxOptions = new FirefoxOptions();
-                firefoxOptions.setBinary(firefoxBinary);
-                //driver = new FirefoxDriver(firefoxOptions);
-            }else if (Navegador == "IE") {
-                Hooks.getInstance().getDriver();
-                //InternetExplorerDriver driver = new InternetExplorerDriver();
-            }
-            new Dimension(1366, 768);
-        } catch (Exception e) {
-            System.out.println("--------ERRO Navegador-----------" + e.getMessage());
-        }
-
-         */
     }
 
 
